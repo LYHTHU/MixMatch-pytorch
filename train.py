29598aafed4d08ffefe82a04586f276d25ab6961
yunhaoli <yunhaoli.cs@gmail.com>
2019-12-19 10:00:13 -0500

    Top accu == 88.6125 with l_u = 1

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ parser.add_argument('--val-iteration', type=int, default=1024,
 parser.add_argument('--out', default='result',
                         help='Directory to output the result')
 parser.add_argument('--alpha', default=0.75, type=float)
-parser.add_argument('--lambda-u', default=75, type=float)
+parser.add_argument('--lambda-u', default=1, type=float)
 parser.add_argument('--T', default=0.5, type=float)
 parser.add_argument('--ema-decay', default=0.999, type=float)
 
